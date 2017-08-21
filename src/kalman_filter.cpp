008d770c3ed51f2f186ce518e0dbcfa567060261
markusbuchholz <30973337+markusbuchholz@users.noreply.github.com>
2017-08-21 18:57:22 +0200

    Update
    Update

---
+++ src/kalman_filter.cpp
@@ -0,0 +1,100 @@
+#include "kalman_filter.h"
+#include <cmath> 
+
+
+using Eigen::MatrixXd;
+using Eigen::VectorXd;
+
+KalmanFilter::KalmanFilter() {}
+
+KalmanFilter::~KalmanFilter() {}
+
+void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
+                        MatrixXd &H_in, MatrixXd &R_in, MatrixXd &Q_in) {
+  x_ = x_in;
+  P_ = P_in;
+  F_ = F_in;
+  H_ = H_in;
+  R_ = R_in;
+  Q_ = Q_in;
+}
+
+void KalmanFilter::Predict() {
+  /**
+    * predict the state
+	where the object wiill be after delta_t
+	x_(2) = F_ * x_(1);
+  */
+  x_ = F_ * x_; 
+  MatrixXd Ft = F_.transpose();
+  P_ = F_ * P_ * Ft + Q_;
+}
+
+void KalmanFilter::Update(const VectorXd &z) {
+  /**
+    * update the state by using Kalman Filter equations
+	 1. The filter compares the "predicted" H_ * x_(2) location with sensor measurement z
+	 2. State update: state/covariance update x_(3) = x_(2) + (K * y)
+	 3. Next cycle: x_(1)=x_(3)
+  */
+
+  
+  VectorXd z_pred = H_ * x_;
+  VectorXd y = z - z_pred;
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd K =  P_ * Ht * Si;
+
+  
+  x_ = x_ + (K * y);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
+}
+
+void KalmanFilter::UpdateEKF(const VectorXd &z) {
+  /**
+     * update the state by using Kalman Filter equations
+	 1. The filter compares the "predicted" H_ * x_(2) location with sensor measurement z
+	 2. State update: state/covariance update x_(3) = x_(2) + (K * y)
+	 3. Next cycle: x_(1)=x_(3)
+  */
+
+   
+  float px = x_(0);
+  float py = x_(1);
+  float vx = x_(2);
+  float vy = x_(3);
+
+  float rho = sqrt(px*px + py*py);
+  float theta = atan2(py, px);
+  float rho_dot = (px*vx + py*vy)/rho;
+
+ 
+  VectorXd h_x = VectorXd(3);
+  h_x << rho, theta, rho_dot;
+  VectorXd y = z - h_x;
+
+
+  if (y(1) > M_PI)
+	{
+		y(1) = y(1) - 2 * M_PI;
+	
+	}
+	else if (y(1) < -M_PI)
+	{
+	  y(1) = y(1) + 2 * M_PI;
+    }
+  
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd K = P_ * Ht * Si;
+  
+  
+  x_ = x_ + (K * y);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
+}
